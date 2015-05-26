103d271d8fbb73c38397893d14c1ff1c7a7802d5
david44869 <david44869@gmail.com>
2015-05-26 20:24:09 +0800

    test
    000

---
+++ app/src/androidTest/java/com/gengtu/gengtu/VideoToGIF.java
@@ -0,0 +1,8 @@
+package com.gengtu.gengtu;
+
+/**
+ * Created by Joseph on 2015/5/23.
+ */
+public class VideoToGIF {
+    //Who wants to add this feature???????????????
+}
---
+++ app/src/androidTest/java/com/gengtu/gengtu/addTextToPhoto.java
@@ -0,0 +1,9 @@
+package com.gengtu.gengtu;
+
+/**
+ * Created by Joseph on 2015/5/23.
+ */
+public class addTextToPhoto {
+    //Who wants to add this feature???????????????
+    //blah blah blah
+}
